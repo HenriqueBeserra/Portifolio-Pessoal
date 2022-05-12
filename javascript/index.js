190f45104cbaff65e7ff1c3b3a3cd11f11d2673b
HenriqueBeserra <76842548+HenriqueBeserra@users.noreply.github.com>
2022-05-12 11:58:48 -0300

    Javascript Interativo da Página

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+
+// Faz a página rolar para baixo
+(function DescePagina() {
+    let icone = document.querySelector('#Icone')
+    icone.addEventListener("click", () => {
+        console.log("DEsceu")
+        window.scrollTo(0,1000)
+    })
+} ) ()
+
