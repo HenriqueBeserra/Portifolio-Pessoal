150bebf93b55082f9c2054c20a9fa4bd916222c0
HenriqueBeserra <76842548+HenriqueBeserra@users.noreply.github.com>
2022-05-12 22:01:21 -0300

    Structure changes

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 
 // Faz a página rolar para baixo
 (function DescePagina() {
-    let icone = document.querySelector('#Icone')
+    let icone = document.querySelector('.footer ')
     icone.addEventListener("click", () => {
         console.log("DEsceu")
         window.scrollTo(0,1500);
